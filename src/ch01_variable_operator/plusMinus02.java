4c015fd1f0589ecb1e2622ff4ad8eb37310574fb
hwaseong <supyoungsun@kakao.com>
2026-02-27 17:57:06 +0900

    plusMinus02 추가

---
+++ src/ch01_variable_operator/plusMinus02.java
@@ -0,0 +1,18 @@
+package ch01_variable_operator;
+
+public class plusMinus02 {
+    static void main(String[] args) {
+        int x= 3, y=5, z;
+        z = x ++ + --y;
+
+        System.out.println("x : "+x);
+        System.out.println("y : "+y);
+        System.out.println("z : "+z);
+
+        z += --x + y++;
+        System.out.println("x : "+x);
+        System.out.println("y : "+y);
+        System.out.println("z : "+z);
+
+     }
+}
